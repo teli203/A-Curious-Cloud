e31c604340087747f7f9f3a0a57feb290814a24b
teli203@aol.com <your.email@example.com>
2024-02-02 23:12:16 -0600

    connected mongoDB

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ const fs = require('fs');
 const jwtSecret = process.env.JWT_SECRET;  //Accessing the .env file //
 const app = express();
 const PORT = process.env.PORT || 5500;
+
+// FAVICON //
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// Connect to MongoDB //
+
+const connectDB = async () => {
+    try {
+        const conn =await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true, 
+        });
+    } catch (error) {}
+};
+
