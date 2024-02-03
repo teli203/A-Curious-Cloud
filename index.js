bf4653b22f26b6df587370abeeafccd2f39ead4f
teli203@aol.com <your.email@example.com>
2024-02-02 23:59:04 -0600

    add login, async func, etc

--- index.js
+++ index.js
@@ -112,7 +112,146 @@ app.post('/register', async (req, res) => {
         role,
     });
 
-    await 
+    await newUser.save();
+    res.status(201).send({ success: true });
 });
 
 
+// User login //
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+  
+    // Sanitze and validate user input //
+
+    const sanitizedUsername = validator.escape(username);
+    const sanitizedPassword = validator.escape(password);
+  
+    // Ensure valid input data //
+
+    if (!sanitizedUsername || !sanitizedPassword) {
+      return res.status(400).send({ error: 'Invalid input data' });
+    }
+  
+    const user = await User.findOne({ username: sanitizedUsername });
+  
+    if (user) {
+      if (bcrypt.compare(password, user.password)) {
+        const accessToken = jwt.sign(
+          { username: user.username, role: user.role },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: '24h',
+          }
+        );
+        res
+          .status(200)
+          .send({ success: true, token: accessToken, role: user.role });
+      } else {
+        res.status(401).send({ success: false });
+      }
+    } else {
+      res.status(401).send({ success: false });
+    }
+  });
+  
+  // Read all posts //
+
+  app.get('/posts', async (req, res) => {
+    const posts = await Post.find();
+    res.status(200).send(posts);
+  });
+  
+  app.post('/posts', authenticateJWT, async (req, res) => {
+    if (req.user.role === 'admin') {
+      const { title, content, imageUrl, author, timestamp } = req.body;
+  
+      const newPost = new Post({
+        title,
+        content,
+        imageUrl,
+        author,
+        timestamp,
+      });
+  
+      newPost
+        .save()
+        .then((savedPost) => {
+          res.status(201).send(savedPost);
+        })
+        .catch((error) => {
+          res.status(500).send({ error: 'Internal Server Error' });
+        });
+    } else {
+      res.sendStatus(403);
+    }
+  });
+  
+  app.get('/post/:id', async (req, res) => {
+    const postId = req.params.id;
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).send('Post not found');
+    }
+  
+    // Read the HTML template from the file //
+
+    fs.readFile(path.join(__dirname, 'post-detail.html'), 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Internal Server Error');
+      }
+  
+      // Replace placeholders in th HTML with actual post data /
+      /
+      const postDetailHtml = data
+        .replace(/\${post.imageUrl}/g, post.imageUrl)
+        .replace(/\${post.title}/g, post.title)
+        .replace(/\${post.timestamp}/g, post.timestamp)
+        .replace(/\${post.author}/g, post.author)
+        .replace(/\${post.content}/g, post.content);
+  
+      res.status(200).send(postDetailHtml);
+    });
+  });
+  
+  // Delete Post //
+
+  app.delete('/posts/:id', authenticateJWT, async (req, res) => {
+    if (req.user.role == 'admin') {
+      try {
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(200).send({ message: 'Post deleted' });
+      } catch (error) {
+        res.status(500).send({ error: 'Internal Server Error' });
+      }
+    } else {
+      res.status(403).send({ error: 'Forbidden' });
+    }
+  });
+  
+  // Update Post //
+  
+  app.put('/posts/:id', authenticateJWT, async (req, res) => {
+    const { title, content } = req.body;
+    const postId = req.params.id;
+  
+    try {
+      const post = await Post.findById(postId);
+  
+      if (!post) {
+        return res.status(404).send({ error: 'Post not found' });
+      }
+  
+      if (req.user.role === 'admin') {
+        post.title = title;
+        post.content = content;
+        await post.save();
+        res.status(200).send(post);
+      } else {
+        res.status(403).send({ error: 'Forbidden' });
+      }
+    } catch (error) {
+      res.status(500).send({ error: 'Internal Server Error' });
+    }
+  });
