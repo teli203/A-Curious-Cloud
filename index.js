35b0f0de1255fa4324ef0d889587188275cf33db
teli203@aol.com <your.email@example.com>
2024-02-02 23:05:22 -0600

    add server side logic

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('running');
+// SERVER SIDE LOGIC //
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const path = require('path');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const fs = require('fs');
+
+const jwtSecret = process.env.JWT_SECRET;  //Accessing the .env file //
+const app = express();
+const PORT = process.env.PORT || 5500;
